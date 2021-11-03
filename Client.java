410d6f98c5dda43e73f4671db32d07131aa3185f
tanhung210601 <akachick999@gmail.com>
2021-11-03 14:33:26 +0700

    tuan8
    s

---
+++ Client.java
@@ -0,0 +1,42 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Scanner nhap=new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Dem tu trong chuoi\n2. Dao chuoi\nexit");
+            String luaChon=nhap.nextLine();
+            // nhap.nextLine();
+            try {
+                Registry reg=LocateRegistry.getRegistry(5555);
+                Interface citf=(Interface) reg.lookup("MyService1");
+                switch (luaChon) {
+                    case "1":
+                        System.out.println("Nhap chuoi: ");
+                        String s=nhap.nextLine();
+                        System.out.println("So tu trong chuoi: "+citf.DemTu(s));
+                        break;
+                    case "2":
+                        System.out.println("Nhap chuoi: ");
+                        String s2=nhap.nextLine();
+                        System.out.println("Chuoi dao nguoc: "+citf.DaoChuoi(s2));
+                        break;
+                    case "exit":
+                        System.exit(0);
+                    default:
+                        System.out.println("Lua chon khong hop le");
+                        break;
+                }
+                
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+        }
+        
+        
+        
+
+    }
+}
---
+++ Implement.java
@@ -0,0 +1,45 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Implement extends UnicastRemoteObject implements Interface{
+
+    public Implement() throws RemoteException
+    {
+
+    }
+    @Override
+    public int DemTu(String s) throws RemoteException {
+         if (s == null) {
+            return -1;
+        }
+        int count = 0;
+        int size = s.length();
+        boolean notCounted = true;
+        for (int i = 0; i < size; i++) {
+            if (s.charAt(i) != ' ' && s.charAt(i) != '\t' 
+                    && s.charAt(i) != '\n') {
+                if(notCounted) {
+                    count++;
+                    notCounted = false;
+                }
+            } else {
+                notCounted = true;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public String DaoChuoi(String s) throws RemoteException {
+        byte[] strAsByteArray = s.getBytes();
+        byte[] result = new byte[strAsByteArray.length];
+        // Store result in reverse order into the
+        // result byte[]
+        for (int i = 0; i < strAsByteArray.length; i++)
+            result[i] = strAsByteArray[strAsByteArray.length - i - 1];
+        String kq=new String(result);
+        
+        return kq;
+    }
+    
+}
